dd5e31714512b2b785b59b5aa6633c008e8e4b86
Liliia Semenenko <forworkinua@gmail.com>
2022-11-22 16:35:38 +0100

    basic testing code

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 (function () {
-    console.log("set up");
   // demo data for local testing
-  var demoData = [
+  let demoData = [
     {
       text_field1: "I can control you",
       text_field2: "Find your new favorites",
@@ -10,7 +9,24 @@
     },
   ];
 
+  // test the dynamic banner locally
   Adform.DynAdsHelper.setDemoData(demoData);
-
+  // autoWire method automatically pushes text and image values to the HTML elements
   Adform.DynAdsHelper.autoWire();
+
+  window.addEventListener("load", (event) => {
+    console.log("The page has fully loaded");
+
+    let banner = document.getElementById('banner');
+    let state = dhtml.getState();
+
+    console.log("The current state is: " + state);
+
+    clickTAGvalue = dhtml.getVar("clickTAG", "https://www.umain.com/"); //banner will receive clickTAG value - if not defined, banner will land to example.com
+    landingpagetarget = dhtml.getVar("landingPageTarget", "_blank"); //landingPageTarget variable enables to change target from Adform system.
+
+    banner.onclick = function() {
+      window.open(clickTAGvalue, landingpagetarget); //when banner is clicked it will open new window directing to clickTAG value
+    }
+  });
 })();
