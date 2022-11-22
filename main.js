daa6d4631eb8bce3cc2770ec58204995b6748dbe
Liliia Semenenko <forworkinua@gmail.com>
2022-11-22 12:01:34 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,16 @@
+(function () {
+    console.log("set up");
+  // demo data for local testing
+  var demoData = [
+    {
+      text_field1: "I can control you",
+      text_field2: "Find your new favorites",
+      text_field4: "UmainFeatures",
+      img_field: "./assets/umain-logo.jpg",
+    },
+  ];
+
+  Adform.DynAdsHelper.setDemoData(demoData);
+
+  Adform.DynAdsHelper.autoWire();
+})();
